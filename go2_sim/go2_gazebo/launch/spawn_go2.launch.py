cdfad98cf88ba28396096537b8452f30cb2ef489
nalishe <nalishe9105@gmail.com>
2025-08-28 05:10:14 +0000

    Adjust robot spawn position, comment out l2 laser joint, and add map files

--- go2_sim/go2_gazebo/launch/spawn_go2.launch.py
+++ go2_sim/go2_gazebo/launch/spawn_go2.launch.py
@@ -30,7 +30,7 @@ def generate_launch_description():
 
     # Position and orientation
     # [X, Y, Z]
-    position = [0, 0.0, 0.7]
+    position = [0, 0.0, 0.45]
     # [Roll, Pitch, Yaw]
     orientation = [0.0, 0.0, 0.0]
     # Base Name or robot
